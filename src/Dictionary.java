892d6a56598e7288a5114eda2dec6109c4cecf31
dvaykhanskiy <dvaykhanskiy@ucvts.org>
2020-03-12 23:15:01 -0400

    coding yuh im coding yuh

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -529,7 +529,6 @@ public class Dictionary {
               doc.insertString(doc.getLength(),"Antonyms\n" ,header );
               doc.insertString(doc.getLength(),"\n1.Antonym " ,null );
           } catch (FileNotFoundException | BadLocationException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
           }
             }
